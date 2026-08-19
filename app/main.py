03d50292d08f9fa20e9f0c9c41d297f9895a1013
Abhimoy-Mukherjee <abhimoymukherjee@gmail.com>
2026-08-19 19:49:33 +0530

    Add URL model and API schemas

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from .database import Base,engine
+from . import models
+
+Base.metadata.create_all(bind=engine)
+
 
 app=FastAPI(title="URL-Shortener API")
 @app.get("/")
